2a5851802751c1adf002354d79d414384b02c71e
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-07-29 16:54:24 +0530

    updates

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/author', (req, res)=>{
-    res.send("Mrinmoy Pal")
-})
 
 // Start the server
 app.listen(PORT, () => {
