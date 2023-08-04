0bf6c7b6f2ac60844c8fe5b1e4e9c5109bd1febf
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-04 13:58:07 +0530

    creates routes for signup and login

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -8,12 +8,16 @@ const DUMMY_USERS = [
       name: "Mrinmoy Pal",
       image: "https://avatars.githubusercontent.com/u/109314855?v=4",
       places: 17,
+      email: "test1@yo.com",
+      password: "Test@123"
     },
     {
       id: 'u2',
       name: "Manoj Mahesh",
       image: "https://avatars.githubusercontent.com/u/42727681?v=4",
       places: 29,
+      email: "test2@yo.com",
+      password: "Tester321"
     }
   ];
 
@@ -25,12 +29,25 @@ const getUsers = (req, res, next) =>{
 }
 
 const signup = (req, res, next) =>{
-    
+    const {name, email, password} = req.body;
+    const userId = uuidv4();
+    const createdUser = {
+        id: userId,
+        name,
+        email,
+        password
+    };
+    DUMMY_USERS.push(createdUser);
+    res.status(201).json({message:"New user added", createdUser})
 }
 
 const login = (req, res, next) =>{
-    
-   
+    const {email, password} = req.body;
+    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
+    if(!identifiedUser || identifiedUser.password !== password){
+        throw new HttpError("No user found for the mail or password", 401);
+    }
+    res.json({message:"User Logged In", identifiedUser})
 }
 
 exports.getUsers = getUsers;
