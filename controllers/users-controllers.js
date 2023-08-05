66849a3c06ab3e5ae3999417785914b5a9d62c3c
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-05 22:29:58 +0530

    checks API inputs for users routes

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,61 +1,65 @@
 const { v4: uuidv4 } = require("uuid");
+const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 
 //dummy userslist data
 const DUMMY_USERS = [
-    {
-      id: 'u1',
-      name: "Mrinmoy Pal",
-      image: "https://avatars.githubusercontent.com/u/109314855?v=4",
-      places: 17,
-      email: "test1@yo.com",
-      password: "Test@123"
-    },
-    {
-      id: 'u2',
-      name: "Manoj Mahesh",
-      image: "https://avatars.githubusercontent.com/u/42727681?v=4",
-      places: 29,
-      email: "test2@yo.com",
-      password: "Tester321"
-    }
-  ];
+  {
+    id: "u1",
+    name: "Mrinmoy Pal",
+    image: "https://avatars.githubusercontent.com/u/109314855?v=4",
+    places: 17,
+    email: "test1@yo.com",
+    password: "Test@123",
+  },
+  {
+    id: "u2",
+    name: "Manoj Mahesh",
+    image: "https://avatars.githubusercontent.com/u/42727681?v=4",
+    places: 29,
+    email: "test2@yo.com",
+    password: "Tester321",
+  },
+];
 
-const getUsers = (req, res, next) =>{
-    if(DUMMY_USERS.length === 0){
-        throw new HttpError("No users found", 404);
-    }
-    res.status(200).json({users: DUMMY_USERS})
-}
+const getUsers = (req, res, next) => {
+  if (DUMMY_USERS.length === 0) {
+    throw new HttpError("No users found", 404);
+  }
+  res.status(200).json({ users: DUMMY_USERS });
+};
 
-const signup = (req, res, next) =>{
-    const {name, email, password} = req.body;
-    const hasUser = DUMMY_USERS.find(u => u.email === email);
-    if(hasUser){
-        throw new HttpError("User alreay exists", 422);
-    };
-    const userId = uuidv4();
-    const createdUser = {
-        id: userId,
-        name,
-        email,
-        password
-    };
-    DUMMY_USERS.push(createdUser);
-    res.status(201).json({message:"New user added", createdUser})
-}
+const signup = (req, res, next) => {
+    const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError("Invalid inputs from user, please check your data",422);
+  }
 
-const login = (req, res, next) =>{
-    const {email, password} = req.body;
-    const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-    if(!identifiedUser || identifiedUser.password !== password){
-        throw new HttpError("No user found for the mail or password", 401);
-    }
-    res.json({message:"User Logged In", identifiedUser})
-}
+  const { name, email, password } = req.body;
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError("User alreay exists", 422);
+  }
+  const userId = uuidv4();
+  const createdUser = {
+    id: userId,
+    name,
+    email,
+    password,
+  };
+  DUMMY_USERS.push(createdUser);
+  res.status(201).json({ message: "New user added", createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError("No user found for the mail or password", 401);
+  }
+  res.json({ message: "User Logged In", identifiedUser });
+};
 
 exports.getUsers = getUsers;
 exports.signup = signup;
 exports.login = login;
-
-
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const { check } = require("express-validator");
 const HttpError = require("../models/http-error");
-const usersControllers = require('../controllers/users-controllers');
+const usersControllers = require("../controllers/users-controllers");
 
 const router = express.Router();
 
 router.get("/", usersControllers.getUsers);
-router.post('/signup', usersControllers.signup);
-router.post('/login', usersControllers.login);
-
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail() // Test@test.com -> test@test.com
+    .isEmail(), // checks if valid email
+    check("password").isLength({ min: 8 }),
+  ],
+  usersControllers.signup
+);
+router.post("/login", usersControllers.login); //validation not need as in login method 
+//inside users-controller there is already check if email matches and pwd matches with data in DB
+//we can still add similar checks as used in signin if we want to
 
 module.exports = router;
