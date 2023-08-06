f1465fde2ded82337aaae4cd0256066b5b9adbc7
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-07 00:36:48 +0530

    Creates placeSchema and Place model

---
+++ models/places.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    address: {type: String, required: true},
+    image: {type: String, required: true},
+    location: {
+        lat: {type: Number, required: true},
+        lan: {type: Number, required: true}
+    },
+    creator: {type: String, required: true},
+});
+
+module.exports = mongoose.model('Place', placeSchema); //covention is to keep 1st letter capital for 'Place'
+//and also it should be in singular number.
