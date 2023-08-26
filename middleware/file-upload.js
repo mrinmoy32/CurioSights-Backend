2cfb54e5ecaaedf36dec87e8b0b2d2f7059921cf
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-26 14:41:34 +0530

    fixes uuidv4

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,5 +1,5 @@
 const multer = require("multer");
-const uuid = require("uuid/v1");
+const { v4: uuidv4 } = require("uuid");
 
 //map MIME_TYPES of file extenstions
 const MIME_TYPE_MAP = {
@@ -17,7 +17,7 @@ const fileUpload = multer({
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
-      cb(null, uuid() + "." + ext); // this generates random filename with correct extension
+      cb(null, uuidv4() + "." + ext); // this generates random filename with correct extension
     },
   }),
   fileFilter: (req, file, cb) => {
