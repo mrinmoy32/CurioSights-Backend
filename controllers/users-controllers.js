41e5a54c2d897be54e64928ebdabdf3aa280b5f0
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-27 16:31:48 +0530

    Login(compare plain pwd with hashed pwd in DB)

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -118,7 +118,7 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError(
       "Invalid credentials, Could not log you in",
       422
@@ -130,6 +130,23 @@ const login = async (req, res, next) => {
   // if (!identifiedUser || identifiedUser.password !== password) {
   //   throw new HttpError("No user found for the mail or password", 401);
   // }
+
+  let isValidPassword = false;
+try {
+  isValidPassword = await bcrypt.compare(password, existingUser.password)
+} catch (err) {
+  const error = new HttpError('Could not log you in, please check you credentials', 500);
+  return next(error);
+}
+
+if(!isValidPassword){
+  const error = new HttpError(
+    "Invalid credentials, Could not log you in",
+    422
+  );
+  return next(error);
+}
+
   res.json({
     message: "User Logged In",
     user: existingUser.toObject({ getters: true }),
