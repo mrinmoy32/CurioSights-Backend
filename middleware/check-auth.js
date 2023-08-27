a3c12a6ca8d2b42506e1e08c39e90d3d6ae4629e
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-27 22:45:59 +0530

    BE route protection with checkAuth middleware

---
+++ middleware/check-auth.js
@@ -0,0 +1,20 @@
+const HttpError = require("../models/http-error");
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; //Authorization: 'Bearer TOKEN',
+    if (!token) {
+      throw new Error("Authentication failed!");
+    }
+    const decodedToken = jwt.verify(token, "super*secret-key_dont_share");
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError(
+      "Authentication failed, no token received",
+      401
+    );
+    return next(error);
+  }
+};
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -4,11 +4,15 @@ const HttpError = require("../models/http-error");
 const placesControllers = require("../controllers/places-controllers");
 const router = express.Router();
 const fileUpload = require('../middleware/file-upload');
+const checkAuth = require("../middleware/check-auth");
 
 router.get("/:placeId", placesControllers.getPlaceById);
 
 //In below route we are adding /user befor :userId to make it different from the :placeId
 router.get("/user/:userId", placesControllers.getPlacesByUserId);
+
+//any route below this checkAuth middleware are protected & can only be reached with a valid token
+router.use(checkAuth);
 
 router.post(
   "/",
