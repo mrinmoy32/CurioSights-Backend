0219fdd3d0d37cfd83bcc2b3970a9e036c71ef01
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-29 21:55:27 +0530

    import & config dotenv path

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const HttpError = require("./models/http-error");
+// Import the dotenv package
+const dotenv = require('dotenv');
+
+// Load environment variables from nodemon.json
+dotenv.config({
+  path: 'nodemon.json'
+});
 
 //Importing placesRoutes, usersRoutes middlewares
 const placesRoutes = require("./routes/places-routes");
