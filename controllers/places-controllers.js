4258b020cdc464fbe8b7db0a1283deaa42bd1cae
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-29 19:50:29 +0530

    Solves id issue

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -147,7 +147,7 @@ const createPlace = async (req, res, next) => {
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (error) {
     return next(
       new HttpError("Creating place failed, user does not exist", 500)
