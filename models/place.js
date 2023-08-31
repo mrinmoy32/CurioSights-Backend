bbd73c886a3cd5127e4c8f6fdbab463e6bc71028
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-09-01 00:47:14 +0530

    fixing loaction obj in Place data model

--- models/place.js
+++ models/place.js
@@ -9,7 +9,7 @@ const placeSchema = new Schema({
     image: {type: String, required: true},
     location: {
         lat: {type: Number, required: true},
-        lan: {type: Number, required: true}
+        lng: {type: Number, required: true}
     },
     creator: {type: mongoose.Types.ObjectId, required: true, ref: 'User'},
 });
